f164a552ae7684bd9c493d0208c6287ee3d6e7dc
fobaj <fobaj.s@googlemail.com>
2020-04-20 19:12:49 +0300

    Tasks A and B.

---
+++ src/akot/Lesson2/B/TaskB3.java
@@ -0,0 +1,9 @@
+package akot.Lesson2.B;
+
+public class TaskB3 {
+    public static void main (String[] args){
+        String s = "С";
+        System.out.println(s + " " + "Новым Годом");
+    }
+
+}
