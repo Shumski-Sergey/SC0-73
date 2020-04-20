ecb2ddd26f08b23724440b3d336c3628caeebe51
Alex <alexandertrofimovich114511@gmail.com>
2020-04-20 22:24:45 +0300

    Homework

---
+++ src/atrofimovich/Lesson1/Lesson1.java
@@ -0,0 +1,11 @@
+package atrofimovich.Lesson1;
+
+public class Lesson1 {
+    public static void main(String[] args){
+        int x=10;
+        while(x>0) {
+            System.out.println("Go, Java!");
+            x-=1;
+        }
+    }
+}
