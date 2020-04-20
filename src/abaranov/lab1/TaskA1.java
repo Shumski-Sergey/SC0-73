32c07d2c2422ff8e638a17eb46923917a3f3bb8e
Alexey <alex003bar@icloud.com>
2020-04-20 19:26:47 +0300

    Lab1

---
+++ src/abaranov/lab1/TaskA1.java
@@ -0,0 +1,7 @@
+package abaranov.lab1;
+
+public class TaskA1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/abaranov/lab1/TaskA3.java
@@ -0,0 +1,10 @@
+package abaranov.lab1;
+
+public class TaskA3 {
+    public static void main(String[] args) {
+        int i = 2;
+        int j = 3;
+        int k = i + j;
+        System.out.println(j+"*"+j+"+"+i*2+"*"+i*2+"="+i+k);
+    }
+}
