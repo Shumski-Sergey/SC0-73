ade40212bf2afa0a111ab94925ac33818cb6dd2b
c0n5n <artem122376@gmail.com>
2020-04-20 21:36:27 +0300

    Changes

---
+++ src/Kononchyk/Lab1.java
@@ -0,0 +1,17 @@
+package Kononchyk;
+import java.util.Scanner;
+
+public class Lab1 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("a:");
+        int a = sc.nextInt();
+        System.out.println("b:");
+        int b = sc.nextInt();
+        int d=a+b;
+        System.out.println(d);
+        System.out.println(Integer.toBinaryString(a)+Integer.toBinaryString(b));
+        System.out.println(Integer.toHexString(a)+Integer.toHexString(b));
+        System.out.println(Integer.toOctalString(a)+Integer.toOctalString(b));
+    }
+}
