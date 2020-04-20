60e34671488ad48152517230b5182f684f2a5085
AlexNiunko <alexniunko89@gmail.com>
2020-04-20 21:33:57 +0300

    Исправленное задание С3.

--- src/alexniunko/С3.java
+++ src/alexniunko/С3.java
@@ -2,18 +2,22 @@ package alexniunko;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.Scanner;
-public class С3 {
+public class С3<WeightMars> {
     public static void main(String[] args) {
         Scanner num = new Scanner(System.in);
-        int WeightHuman;
-        double WeightMars;
+        double a;
         System.out.println("Введите массу человека на Земле:");
-        WeightHuman = num.nextInt();
-        WeightMars=(3.86/9.81)*WeightHuman;
-        BigDecimal a=new BigDecimal(WeightMars);
-        MathContext m = new MathContext(4);
-        BigDecimal a2=a.round(m);
-        System.out.println("Масса человека на Марсе : " + a2);
+        a = num.nextInt();
+        System.out.println(getWeight(a));
+    }
+    public static double getWeight(double weightEarth) {
+        double a = weightEarth;
+        double b = (3.86 / 9.81) * a;
+        double weightMars = new BigDecimal(b).setScale(2, RoundingMode.UP).doubleValue();
+        System.out.println("Вес человека на Марсе в кг ");
+        return weightMars;
     }
 }
+
