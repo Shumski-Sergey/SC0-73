41eb89ce0c63c7778d77eb56d86a473d83570c03
fobaj <fobaj.s@googlemail.com>
2020-04-21 21:59:23 +0300

    Lesson 3 Tasks A complete.

---
+++ src/akot/Lesson3/A/TaskA2.java
@@ -0,0 +1,14 @@
+package akot.Lesson3.A;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class TaskA2 {
+    public static void main(String[] args) {
+        String a = "Темный лес и берег пруда,";
+        String b = "Ели сваленной бревно.";
+        String c = "Путь отрезан отовсюду.";
+        String d = "Будь что будет, все равно.";
+        System.out.println("Моё любимое стихотворение:");
+        System.out.println(a + "\n" + b + "\n" + c + "\n" + d);
+    }
+}
