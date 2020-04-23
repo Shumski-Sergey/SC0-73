f7041125dfc3d50bab61d2049dd429221a7e54d9
fobaj <fobaj.s@googlemail.com>
2020-04-24 00:41:03 +0300

    Added Lesson 3 Task B2.

---
+++ src/akot/Lesson3/B/TaskB2.java
@@ -0,0 +1,12 @@
+package akot.Lesson3.B;
+
+public class TaskB2 {
+    public static void main(String[] args) {
+        double d = 111.111;
+        byte b = 0b111;
+        long o = 0111;
+        int h = 0x111;
+        float f = (float) 111.111;
+        System.out.println(d + " " + b + " " + o + " " + h + " " + f);
+    }
+}
