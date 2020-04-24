12b37a928c283810fe8463fa77ae0e7d576b7d39
Alex <alexandertrofimovich114511@gmail.com>
2020-04-24 19:11:50 +0300

    NewHomework

---
+++ src/atrofimovich/Lesson3/TaskC2.java
@@ -0,0 +1,21 @@
+package atrofimovich.Lesson3;
+
+import java.util.Scanner;
+
+//public class TaskC2 {
+//    public static void main(String[] args){
+//        System.out.println("Введите целое число:");
+//        Scanner sc = new Scanner(System.in);
+//        int a;
+//        a = sc.nextInt();
+//        int b = sumDigitsInNumber(a);
+//    }
+//
+//    private static int sumDigitsInNumber(int number){
+//        while(number != 0){
+//            number = number%10;
+//        }
+//        int sum;
+//        return sum;
+//    }
+//}
