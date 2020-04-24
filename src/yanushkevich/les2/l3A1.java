8cadef70768979401ac1044ce206a204ccbddc38
wjanuszkiewicz <wjanuszkiewicz5@gmail.com>
2020-04-24 21:01:05 +0200

    Les3 lab.

--- src/yanushkevich/les2/l3A1.java
+++ src/yanushkevich/les2/l3A1.java
@@ -1,8 +1,7 @@
 package yanushkevich.les2;
 
 import java.util.Scanner;
-
-public class les3A1 {
+   public class l3A1 {
     public static void main(String[] args){
         Scanner k = new Scanner(System.in);
         int a = k.nextInt();
@@ -19,7 +18,5 @@ public class les3A1 {
         double ddl = (double) a / b;
         double mmd = a % b;
         System.out.println(ssm + " " + rraz + " " + mmn + " " + ddl + " " +mmd);
-
-
     }
 }
