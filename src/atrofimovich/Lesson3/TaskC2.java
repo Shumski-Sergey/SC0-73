2569040ab21b2365667312670059994fe4a805dc
Alex <alexandertrofimovich114511@gmail.com>
2020-04-25 16:07:28 +0300

    NewHomework

--- src/atrofimovich/Lesson3/TaskC2.java
+++ src/atrofimovich/Lesson3/TaskC2.java
@@ -2,20 +2,21 @@ package atrofimovich.Lesson3;
 
 import java.util.Scanner;
 
-//public class TaskC2 {
-//    public static void main(String[] args){
-//        System.out.println("Введите целое число:");
-//        Scanner sc = new Scanner(System.in);
-//        int a;
-//        a = sc.nextInt();
-//        int b = sumDigitsInNumber(a);
-//    }
-//
-//    private static int sumDigitsInNumber(int number){
-//        while(number != 0){
-//            number = number%10;
-//        }
-//        int sum;
-//        return sum;
-//    }
-//}
+public class TaskC2{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите целое число:");
+        int a = sc.nextInt();
+        int b = sumDigitsInNumber(a);
+        System.out.println("Сумма всех цифр числа равна: " + b);
+    }
+
+    private static int sumDigitsInNumber(int number){
+        int res = 0;
+        while(number != 0){
+            res = res + number%10;
+            number = number/10;
+        }
+        return res;
+    }
+}
