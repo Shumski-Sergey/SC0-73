b32fec556a099cc0bf81aebc6a8e0c94b9b801ef
fobaj <fobaj.s@googlemail.com>
2020-04-25 17:12:44 +0300

    Added Lesson 4 Task A.

---
+++ src/akot/Lesson4/A/TaskA3.java
@@ -0,0 +1,24 @@
+package akot.Lesson4.A;
+
+import java.util.Scanner;
+
+public class TaskA3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        if (a == b && b == c) {
+            System.out.println(a + " " + b + " " + c);
+        }
+        else if (a == b) {
+            System.out.println(a + " " + b);
+        }
+        else if (b == c) {
+            System.out.println(b + " " + c);
+        }
+        else if (a == c) {
+            System.out.println(a + " " + c);
+        }
+    }
+}
