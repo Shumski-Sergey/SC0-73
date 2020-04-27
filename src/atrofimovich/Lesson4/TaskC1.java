88ccead08437f0e8e25c2d09f17783f647091538
Alex <alexandertrofimovich114511@gmail.com>
2020-04-27 18:19:23 +0300

    Homework to lesson №4 (C1)

---
+++ src/atrofimovich/Lesson4/TaskC1.java
@@ -0,0 +1,29 @@
+package atrofimovich.Lesson4;
+
+import java.util.Scanner;
+
+public class TaskC1 {
+    public static void main(String[] args) {
+        System.out.println("Какая зарплата вас бы устроила?");
+        Scanner sc = new Scanner(System.in);
+        int ilike$ = sc.nextInt();
+        if (ilike$ < 300 || ilike$ > 3000) {
+            System.out.println("Мы вам перезвоним!");
+        } else {
+            for (int month = 0; month < 15; month++) {
+                if (month == 0 || month == 13 || month == 14) {
+                    System.out.println("Ваша зарплата в этом месяце составила: $0.0");
+                } else if (month < 6 || month > 8) {
+                    System.out.println("Ваша зарплата в этом месяце составила: $" + ilike$ * 1.5);
+                } else {
+                    switch (month) {
+                        case 6 -> System.out.println("Ваша зарплата в этом месяце составила: июнь $" + ilike$);
+                        case 7 -> System.out.println("Ваша зарплата в этом месяце составила: июль $" + ilike$);
+                        case 8 -> System.out.println("Ваша зарплата в этом месяце составила: август $" + ilike$);
+                    }
+                }
+            }
+        }
+    }
+}
+//не доделал п. с $666
