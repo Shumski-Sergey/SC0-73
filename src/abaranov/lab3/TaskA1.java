4704945c1aa9446c2c635bf78da68d78f0457e46
Alexey <alex003bar@icloud.com>
2020-04-27 18:46:47 +0300

    Lab3

---
+++ src/abaranov/lab3/TaskA1.java
@@ -0,0 +1,14 @@
+package abaranov.lab3;
+
+import java.util.Scanner;
+
+public class TaskA1 {
+     public static void main (String[] args ) {
+      for (int a = 1; a <= 10; a++) {
+           if (a == 3 || a == 9)
+           System.out.println("Меня интересует высокая зарплата!");
+            else System.out.println("Я изучаю программирование");
+        }
+
+        }
+}
