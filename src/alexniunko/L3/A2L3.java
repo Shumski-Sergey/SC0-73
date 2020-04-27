dd1654659e5938be80ad5514c86f10d39c259162
AlexNiunko <alexniunko89@gmail.com>
2020-04-27 23:20:05 +0300

    Лабораторная работа 3. Задание А2.

---
+++ src/alexniunko/L3/A2L3.java
@@ -0,0 +1,12 @@
+package alexniunko.L3;
+
+public class A2L3 {
+    public static void main(String[] args) {
+        String a="Ты спеши, ты спеши ко мне";
+        String b="Если я вдали, если трудно мне";
+        String c="Если я – словно в страшном сне";
+        String d="Если тень беды в моем окне";
+        System.out.println("Мое любимое стихотворение:");
+        System.out.println(a+"\n"+b+"\n"+c+"\n"+d);
+    }
+}
