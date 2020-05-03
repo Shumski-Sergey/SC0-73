06fc30db04a5482aed819d1d695c67b9af12e0db
Gordislava <gordislava@gmail.com>
2020-05-03 21:00:25 +0300

    Added B1 tasks after 5 lesson

--- src/yponomareva/lesson5/A2.java
+++ src/yponomareva/lesson5/A2.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 public class A2 {
     public static void main(String[] args) throws IOException {
         ArrayList<String> words = new ArrayList<String>();
+        System.out.println("Введите какие-нибудь строки: ");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         while (true) {
             String word = reader.readLine();
@@ -15,6 +16,6 @@ public class A2 {
                 break;
             words.add(word);
         }
-        System.out.print(words);
+        System.out.print("Ваш список: ".concat(String.valueOf(words)));
     }
 }
---
+++ src/yponomareva/lesson5/B1.java
@@ -0,0 +1,24 @@
+package yponomareva.lesson5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class B1 {
+    public static void main(String[] args) throws IOException {
+        ArrayList<String> strings = new ArrayList<String>();
+        System.out.println("Введите 5 строк: ");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0;i < 5;i++) {
+            String word = reader.readLine();
+            strings.add(word);
+        }
+        System.out.println("Строки из вашей коллекции: ");
+        for (String x: strings) {
+            System.out.println(x);
+        }
+        strings.size();
+        System.out.print("Размер вашей коллекции: ".concat(String.valueOf(strings.size())));
+    }
+}
